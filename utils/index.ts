a10549b3a8d9cbee1988a2b7d516bcb259cdcc1e
Kasuni Abeynayake <135317655+KasuniGA@users.noreply.github.com>
2025-08-03 22:45:07 +0530

    Add search functionality and filters to the car catalogue page; implement CustomFilter and SearchBar components

---
+++ utils/index.ts
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  params: {model: 'corolla'},
+  headers: {
+    'x-rapidapi-key': '18fbdf89c1msh1dfe0dc9174c652p1b7044jsn295af5373cce',
+    'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+};
+
+async function fetchData() {
+	try {
+		const response = await axios.request(options);
+		console.log(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchData();
